79440b6d32bebbe0cceafcf7e69f52cd238b411f
Jeremy <2696048261@qq.com>
2024-04-09 16:56:39 +0800

    添加过滤器

--- routers/router.go
+++ routers/router.go
@@ -6,9 +6,18 @@ import (
 	"CourseSystem/controllers/StudentControllers"
 	"CourseSystem/controllers/TeacherControllers"
 	beego "github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web/context"
 )
 
 func init() {
+	// 在 Beego 中注册过滤器
+	var FilterUser = func(ctx *context.Context) {
+		if ctx.Request.RequestURI != "/login" {
+			ctx.Redirect(302, "/login")
+		}
+	}
+
+	beego.InsertFilter("/*", beego.BeforeRouter, FilterUser)
 	beego.Router("/", &controllers.BaseController{})
 	beego.Router("/stu/:stuid", &StudentControllers.StudentController{})
 	beego.Router("/tea/:teaid", &TeacherControllers.TeacherController{})
